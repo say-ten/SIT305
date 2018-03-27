e06d36e6974a1a32e7b84fd2bbe801274617e7c7
say-ten <anthonyea@hotmail.com>
2018-03-27 19:05:00 +1100

    Update character.cs
    Added losing hp when attacked and game over message

--- Character.cs
+++ Character.cs
@@ -10,5 +10,18 @@ public class Character : MonoBehaviour
     public int Defence { get; set; }
     public Vector2 RoomIndex { get; set; }
     public List<string> Inventory { get; set; }
+}
 
+public virtual void Attacked(int hp)
+{
+    Health -= hp;
+    if (Health <= 0)
+    {
+        Dead();
+    }
+}
+
+public virtual void Dead()
+{
+    Debug.Log("Game Over, You have died.");
 }
