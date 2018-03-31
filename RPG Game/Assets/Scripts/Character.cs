496a8f263012edbefb73a05af4dc145f31cb3238
Anthony Ea <anthonyea@hotmail.com>
2018-03-31 20:14:27 +1100

    Formatting
    Fixed formatting for unity

---
+++ RPG Game/Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGGame
+{
+    public class Character : MonoBehaviour
+    {
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public Vector2 RoomIndex { get; set; }
+        public List<string> Inventory { get; set; }
+    }
+
+    public virtual void Attacked(int hp)
+    {
+        Health -= hp;
+        if (Health <= 0)
+        {
+            Dead();
+        }
+    }
+
+    public virtual void Dead()
+    {
+        Debug.Log("Game Over, You have died.");
+    }
+}
