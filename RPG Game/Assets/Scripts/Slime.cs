ef41031655d33d64fa10349756b438048e2c7c89
Anthony Ea <anthonyea@hotmail.com>
2018-04-09 17:14:17 +1000

    Decided on monsters

---
+++ RPG Game/Assets/Scripts/Slime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGGame
+{
+    public class Slime : Monster
+    {
+        void Start()
+        {
+            Health = 0;
+            Attack = 0;
+            Defence = 0;
+            Inventory.Add("Slime monster core");
+        }
+    }
+}
