7871e1bb351044246082cafecc6de461a3def52c
Alex Ter <italexter@gmail.com>
2018-04-16 15:17:41 +1000

    Chest Class
    Added item chest feature to dungeon.

---
+++ RPG Game/Assets/Scripts/Chest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGGame
+{
+    public class Chest : MonoBehaviour
+    {
+        public Monster Monster { get; set}
+        public string Item { get; set; }
+        public bool Trap { get; set; }
+        public bool Heal { get; set; }
+
+        public Chest()
+        {
+            if (Random.Range(0,7) == 2)
+            {
+                Trap = true;
+            }
+            else if (Random.Range(0,5) == 2)
+            {
+                Heal = true;
+            }
+            else if(Random.Range(0,5) == 2)
+            {
+                Monster = MonsterDatabase.Instance.Monsters[Random.Range(0, MonsterDatabase.Instance.Monsters.Count)];
+            }
+            else
+            {
+                //int itemToAdd = Random.Range(0, ItemDatabase.Instance.Items.Count);
+                //Item = ItemDatabase.Instance.Items[itemToAdd];
+            }
+        }
+    }
+}
