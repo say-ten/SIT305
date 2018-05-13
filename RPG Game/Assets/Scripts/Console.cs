5a83900200a6ffdd7ecee9095bd734d9c04163d4
Anthony Ea <anthonyea@hotmail.com>
2018-05-13 18:47:34 +1000

    Added Text Console

---
+++ RPG Game/Assets/Scripts/Console.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPGGame
+{
+    public class Console : MonoBehaviour
+    {
+        [SerializeField] Text logText;
+        public static Console Instance { get; set; }
+
+        void Awake()
+        {
+            if (Instance != null && Instance != this)
+                Destroy(this.gameObject);
+            else
+                Instance = this;
+        }
+
+        public void Entry(string text)
+        {
+            logText.text += "\n" + text;
+        }
+    }
+}
